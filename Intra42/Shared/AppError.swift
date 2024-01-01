c342d4e66a3fca0868ac38aab121b5d8edccc16d
Marc Mosca <marc.mosca@icloud.com>
2024-01-01 13:58:28 +0100

    feat(api): implement api client class

--- Intra42/Shared/AppError.swift
+++ Intra42/Shared/AppError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+/// An object representing errors related to the application.
+enum AppError: LocalizedError
+{
+    
+    case apiAuthorization
+    case network
+    
+    var errorDescription: String?
+    {
+        switch self
+        {
+        case .apiAuthorization:
+            return String(localized: "An error has occurred with the authorization to share your data.")
+        case .network:
+            return String(localized: "An error has occurred with the network. Please try again later")
+        }
+    }
+    
+}
