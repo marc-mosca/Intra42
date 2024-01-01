64adad389f3f177704535c54a9460e73778acc99
Marc Mosca <marc.mosca@icloud.com>
2024-01-01 11:55:40 +0100

    feat(api): implement scale row ui component

---
+++ Intra42/Api/UI Components/Rows/ScaleRow.swift
@@ -0,0 +1,57 @@
+//
+//  ScaleRow.swift
+//  Intra42
+//
+//  Created by Marc Mosca on 01/01/2024.
+//
+
+import SwiftUI
+
+struct ScaleRow: View
+{
+    
+    // MARK: - Exposed properties
+    
+    let scale: Api.Types.Scale
+    
+    // MARK: - Private properties
+    
+    private var title: String
+    {
+        String(localized: "You will be evaluated by someone on ft_transcendence in 45 minutes.")
+    }
+    
+    private var formatStyle: Date.FormatStyle
+    {
+        .dateTime.day().month().year().hour().minute()
+    }
+    
+    // MARK: - Body
+    
+    var body: some View
+    {
+        HStack(spacing: 16)
+        {
+            Image(systemName: "person.badge.clock")
+                .foregroundStyle(.night)
+                .font(.headline)
+                .imageScale(.large)
+            
+            VStack(alignment: .leading, spacing: 2)
+            {
+                Text(title)
+                    .foregroundStyle(.primary)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.vertical, 8)
+    }
+}
+
+// MARK: - Previews
+
+#Preview
+{
+    ScaleRow(scale: .sample)
+}
