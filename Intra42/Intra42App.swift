9bea8ddca6025b967ffba941c64bfbf1e59836b3
Marc Mosca <marc.mosca@icloud.com>
2024-01-01 10:44:03 +0100

    Initial Commit

---
+++ Intra42/Intra42App.swift
@@ -0,0 +1,17 @@
+//
+//  Intra42App.swift
+//  Intra42
+//
+//  Created by Marc Mosca on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Intra42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
