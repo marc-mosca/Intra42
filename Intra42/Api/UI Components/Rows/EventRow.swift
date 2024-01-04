fe8bf19caa494ebf754b07f79b925963b50da449
Marc Mosca <marc.mosca@icloud.com>
2024-01-04 10:02:48 +0100

    fix(campus): unsubscribe and subscribe methods

--- Intra42/Api/UI Components/Rows/EventRow.swift
+++ Intra42/Api/UI Components/Rows/EventRow.swift
@@ -14,107 +14,17 @@ struct EventRow: View
     
     let event: Api.Types.Event
     
-    // MARK: - Private properties
-    
-    @Environment(\.store) private var store
-    @Environment(\.dismiss) private var dismiss
-    @AppStorage("userIsConnected") private var userIsConnected: Bool?
-    @State private var showAlert = false
-    
-    private var userIsSubscribe: Bool
-    {
-        store.userEvents.contains(where: { $0.id == event.id })
-    }
-    
-    private var userIsSubscribeFormatted: String
-    {
-        userIsSubscribe ? String(localized: "Yes") : String(localized: "No")
-    }
-    
     // MARK: - Body
     
     var body: some View
     {
         NavigationLink
         {
-            List
-            {
-                Section("Informations")
-                {
-                    HRow(title: "Date", value: event.beginAt)
-                    HRow(title: "Duration", value: Date.duration(beginAt: event.beginAt, endAt: event.endAt))
-                    HRow(title: "Registered", value: userIsSubscribeFormatted)
-                    HRow(title: "Subscribers", value: event.numberOfSubscribers)
-                    HRow(title: "Location", value: event.location)
-                }
-                
-                Section("Description")
-                {
-                    Text(event.description)
-                }
-            }
-            .navigationTitle(event.name)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar
-            {
-                ToolbarItem
-                {
-                    Button(userIsSubscribe ? "Unsubscribe" : "Subscribe", action: { showAlert.toggle() })
-                        .disabled(event.hasWaitlist)
-                }
-            }
-            .alert(userIsSubscribe ? "Unsubscribe from the event" : "Subscribe to the event", isPresented: $showAlert)
-            {
-                Button("Cancel", role: .cancel) { }
-                Button(userIsSubscribe ? "Unsubscribe" : "Subscribe", action: handleUpdateEvents)
-            }
-            message:
-            {
-                Text(
-                    userIsSubscribe ? "Do you really want to unsubscribe from this event?"
-                    : "Do you really like to subscribe for this event?"
-                )
-            }
+            RowDestination(event: event)
         }
         label:
         {
             RowLabel(event: event)
-        }
-    }
-    
-    // MARK: - Private methods
-    
-    private func handleUpdateEvents()
-    {
-        guard let user = store.user else { return }
-        
-        Task
-        {
-            do
-            {
-                switch userIsSubscribe
-                {
-                case true:
-                    let eventUser = try await Api.Client.shared.request(for: .fetchEventUser(userId: user.id, eventId: event.id)) as Api.Types.EventUser
-                    try await Api.Client.shared.request(for: .deleteEvent(eventUserId: eventUser.eventId))
-                    store.userEvents.removeAll(where: { $0.id == event.id })
-                case false:
-                    try await Api.Client.shared.request(for: .updateUserEvents(userId: user.id, eventId: event.id))
-                    store.userEvents.append(event)
-                }
-            }
-            catch AppError.apiAuthorization
-            {
-                store.error = .apiAuthorization
-                store.errorAction = {
-                    Api.Keychain.shared.clear()
-                    userIsConnected = false
-                }
-            }
-            catch
-            {
-                store.error = .network
-            }
         }
     }
     
@@ -163,6 +73,124 @@ extension EventRow
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding(.vertical, 8)
+        }
+        
+    }
+    
+    struct RowDestination: View
+    {
+        
+        // MARK: - Exposed properties
+        
+        let event: Api.Types.Event
+        
+        // MARK: - Private properties
+        
+        @Environment(\.store) private var store
+        @AppStorage("userIsConnected") private var userIsConnected: Bool?
+        @State private var showAlert = false
+        
+        private var userIsSubscribe: Bool
+        {
+            store.userEvents.contains(where: { $0.id == event.id })
+        }
+        
+        private var userIsSubscribeFormatted: String
+        {
+            userIsSubscribe ? String(localized: "Yes") : String(localized: "No")
+        }
+        
+        // MARK: - Body
+        
+        var body: some View
+        {
+            List
+            {
+                Section("Informations")
+                {
+                    HRow(title: "Date", value: event.beginAt)
+                    HRow(title: "Duration", value: Date.duration(beginAt: event.beginAt, endAt: event.endAt))
+                    HRow(title: "Registered", value: userIsSubscribeFormatted)
+                    HRow(title: "Subscribers", value: event.numberOfSubscribers)
+                    HRow(title: "Location", value: event.location)
+                }
+                
+                Section("Description")
+                {
+                    Text(event.description)
+                }
+            }
+            .navigationTitle(event.name)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar
+            {
+                ToolbarItem
+                {
+                    Button(userIsSubscribe ? "Unsubscribe" : "Subscribe")
+                    {
+                        showAlert.toggle()
+                    }
+                }
+            }
+            .alert(userIsSubscribe ? "Unsubscribe from the event" : "Subscribe to the event", isPresented: $showAlert)
+            {
+                Button("Cancel", role: .cancel) { }
+                Button(userIsSubscribe ? "Unsubscribe" : "Subscribe", action: handleUpdateEvents)
+            }
+        message:
+            {
+                Text(
+                    userIsSubscribe ? "Do you really want to unsubscribe from this event?"
+                    : "Do you really like to subscribe for this event?"
+                )
+            }
+        }
+        
+        // MARK: - Private methods
+        
+        private func handleUpdateEvents()
+        {
+            guard let user = store.user else { return }
+            
+            Task
+            {
+                do
+                {
+                    switch userIsSubscribe
+                    {
+                    case true:
+                        try await unsubscribe(user: user, event: event)
+                    case false:
+                        try await subscribe(user: user, event: event)
+                    }
+                }
+                catch AppError.apiAuthorization
+                {
+                    store.error = .apiAuthorization
+                    store.errorAction = {
+                        Api.Keychain.shared.clear()
+                        userIsConnected = false
+                    }
+                }
+                catch
+                {
+                    store.error = .network
+                }
+            }
+        }
+        
+        private func unsubscribe(user: Api.Types.User, event: Api.Types.Event) async throws
+        {
+            let eventUser = try await Api.Client.shared.request(for: .fetchEventUser(userId: user.id, eventId: event.id)) as [Api.Types.EventUser]
+            guard let eventUserId = eventUser.first?.id else { throw AppError.network }
+            try await Api.Client.shared.request(for: .deleteEvent(eventUserId: eventUserId))
+            store.userEvents.removeAll(where: { $0.id == event.id })
+        }
+        
+        private func subscribe(user: Api.Types.User, event: Api.Types.Event) async throws
+        {
+            try await Api.Client.shared.request(for: .updateUserEvents(userId: user.id, eventId: event.id))
+            store.userEvents.append(event)
         }
         
     }
