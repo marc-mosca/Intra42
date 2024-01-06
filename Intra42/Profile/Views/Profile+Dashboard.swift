16c1a5575484eaed2d1ef0d3dd9e49b6cc386037
Marc Mosca <marc.mosca@icloud.com>
2024-01-06 10:22:30 +0100

    feat(profile): implement profile dashboard component

---
+++ Intra42/Profile/Views/Profile+Dashboard.swift
@@ -0,0 +1,103 @@
+//
+//  Profile+Dashboard.swift
+//  Intra42
+//
+//  Created by Marc Mosca on 06/01/2024.
+//
+
+import SwiftUI
+
+extension ProfileView
+{
+    
+    struct Dashboard: View
+    {
+        
+        // MARK: - Exposed properties
+        
+        let user: Api.Types.User
+        let isSearchedProfile: Bool
+        
+        // MARK: - Private properties
+        
+        private var listSize: CGFloat
+        {
+            70 * CGFloat(integerLiteral: isSearchedProfile ? 5 : 8)
+        }
+        
+        // MARK: - Body
+        
+        var body: some View
+        {
+            VStack(alignment: .leading, spacing: 20)
+            {
+                Text("Dashboard")
+                    .foregroundStyle(.primary)
+                    .font(.headline)
+                
+                List
+                {
+                    DashboardLink(image: "info.circle", title: "Informations", destination: EmptyView.init)
+                    DashboardLink(image: "briefcase", title: "Projects", destination: EmptyView.init)
+                    
+                    if !isSearchedProfile
+                    {
+                        DashboardLink(image: "calendar", title: "Events", destination: EmptyView.init)
+                        DashboardLink(image: "clock", title: "Logtime", destination: EmptyView.init)
+                        DashboardLink(image: "scroll", title: "Corrections", destination: EmptyView.init)
+                    }
+                    
+                    DashboardLink(image: "list.bullet.clipboard", title: "Skills", destination: EmptyView.init)
+                    DashboardLink(image: "graduationcap", title: "Achievements", destination: EmptyView.init)
+                    DashboardLink(image: "person.2", title: "Patronages", destination: EmptyView.init)
+                }
+                .listStyle(.plain)
+                .frame(minHeight: listSize)
+            }
+        }
+        
+        // MARK: - Private components
+        
+        private struct DashboardLink<T: View>: View
+        {
+            
+            // MARK: - Exposed properties
+            
+            let image: String
+            let title: String.LocalizationValue
+            let destination: () -> T
+            
+            // MARK: - Body
+            
+            var body: some View
+            {
+                NavigationLink
+                {
+                    destination()
+                }
+                label:
+                {
+                    HStack(spacing: 16)
+                    {
+                        Image(systemName: image)
+                            .foregroundStyle(.night)
+                            .imageScale(.medium)
+                            .font(.headline)
+                            .frame(width: 48, height: 48)
+                            .background(.thickMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                        
+                        Text(String(localized: title))
+                            .foregroundStyle(.primary)
+                            .font(.system(.subheadline, weight: .semibold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                }
+            }
+            
+        }
+        
+    }
+    
+}
--- Intra42/Profile/Views/ProfileView.swift
+++ Intra42/Profile/Views/ProfileView.swift
@@ -27,18 +27,22 @@ struct ProfileView: View
     {
         NavigationStack
         {
-            VStack(spacing: 40)
+            ScrollView
             {
-                HStack(spacing: 20)
+                VStack(spacing: 40)
                 {
-                    Avatar(url: user.image.link, isConnected: user.location != nil)
-                    Informations(name: user.displayname, email: user.email, isPostCC: user.postCC, cursus: user.mainCursus)
+                    HStack(spacing: 20)
+                    {
+                        Avatar(url: user.image.link, isConnected: user.location != nil)
+                        Informations(name: user.displayname, email: user.email, isPostCC: user.postCC, cursus: user.mainCursus)
+                    }
+                    
+                    GridInformations(location: user.location, grade: user.mainCursus?.grade, poolYear: user.poolYear)
+                    Dashboard(user: user, isSearchedProfile: isSearchedProfile)
                 }
-                
-                GridInformations(location: user.location, grade: user.mainCursus?.grade, poolYear: user.poolYear)
+                .padding()
             }
             .navigationTitle(isSearchedProfile ? "\(user.login.capitalized)'s profile" : "My profile")
-            .padding()
         }
     }
     
